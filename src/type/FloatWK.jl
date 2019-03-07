9a5d248a6c1baf515dd277994a6ec91fd7fc4922
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-07 02:40:23 -0500

    Update FloatWK.jl

--- src/type/FloatWK.jl
+++ src/type/FloatWK.jl
@@ -1,11 +1,11 @@
 primitive type FloatWK <: AbstractFloat 64 end
 
-FloatWK(x::Float64) = reinterpret(FloatWK, x)
-FloatWK(x::Float32) = reinterpret(FloatWK, Float64(x))
+@inline FloatWK(x::Float64) = reinterpret(FloatWK, x)
+@inline FloatWK(x::Float32) = reinterpret(FloatWK, Float64(x))
 FloatWK(x::BigFloat) = FloatWK(Float64(x))
 
-Float64(x::FloatWK) = reinterpret(Float64, x)
-Float32(x::FloatWK) = Float32(reinterpret(Float64, x))
+@inline Float64(x::FloatWK) = reinterpret(Float64, x)
+@inline Float32(x::FloatWK) = Float32(reinterpret(Float64, x))
 BigFloat(x::FloatWK) = BigFloat(Float64(x))
 
 convert(::Type{FloatWK}, x::Float64) = FloatWK(x)
