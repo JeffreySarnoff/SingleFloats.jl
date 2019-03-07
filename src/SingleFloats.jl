600216cc3e7378f377b3f6d0eea6e5094b66e229
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-07 06:16:40 -0500

    Update SingleFloats.jl

--- src/SingleFloats.jl
+++ src/SingleFloats.jl
@@ -2,7 +2,17 @@ module SingleFloats
 
 export FloatWK
 
-import Base: IEEEFloat, promote_rule, convert 
+using Base: IEEEFloat
+import Base: promote_rule, convert
+
+using LinearAlgebra
+import LinearAlgebra: rank, cond, norm, opnorm, det, tr, inv, pinv. 
+                      transpose, adjoint, eigvals, eigvals!, svdvals, svdvals!, svd,
+                      iszero, isone, isdiag, issquare, issymmetric, ishermitian,
+                      isposdef, isposdef!, istril, istriu,
+                      lu, lu!, qr, qr!,
+                      schur, schur!, hessenberg, hessenberg!,
+                      cholesky, cholesky!, factorize
 
 include("type/FloatWK.jl")
 include("math/FloatWK.jl")
