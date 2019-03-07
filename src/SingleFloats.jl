6350ef2bc8ceabf6fb4c292bd1eabec829231e02
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-07 06:30:15 -0500

    Update SingleFloats.jl

--- src/SingleFloats.jl
+++ src/SingleFloats.jl
@@ -8,7 +8,7 @@ import Base: promote_rule, convert
 using LinearAlgebra
 import LinearAlgebra: rank, cond, norm, opnorm, det, tr, inv, pinv, 
                       transpose, adjoint, eigvals, eigvals!, svdvals, svdvals!, svd,
-                      iszero, isone, isdiag, issquare, issymmetric, ishermitian,
+                      iszero, isone, isdiag, issymmetric, ishermitian,
                       isposdef, isposdef!, istril, istriu,
                       lu, lu!, qr, qr!,
                       schur, schur!, hessenberg, hessenberg!,
