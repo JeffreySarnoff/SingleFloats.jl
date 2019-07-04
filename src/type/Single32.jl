63c4c238ac02096e8ba35b766f3e2d0ff04555fd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-04 04:28:03 -0400

    Update Single32.jl

--- src/type/Single32.jl
+++ src/type/Single32.jl
@@ -1,3 +1,5 @@
+import Base: cmp, ==, !=, >=, >, <=, <, isless, isequal
+
 primitive type Single32 <: AbstractFloat 64 end
 
 const IEEEFloats = Union{Single32, Float64, Float32, Float16}
@@ -32,3 +34,26 @@ Base.repr(x::Single32) = string("Single32(",repr(Float64(x)),")")
 Base.hash(x::Single32) = hash(Float64(x))
 Base.hash(x::Single32, h::UInt64) = hash(Float64(x), h)
 
+
+# comparisons
+for Op in (:cmp, :(==), :(!=), :(>=), :(<=), :(>), :(<), :isless, :isequal)
+    @eval begin
+        $Op(x::Single32, y::Single32) = $Op(Float64(x), Float64(y))
+        $Op(x::Single32, y::Float64) = $Op(Float64(x), y)
+        $Op(x::Float64, y::Single32) = $Op(x, Float64(y))
+        $Op(x::Single32, y::Float32) = $Op(Float64(x), Float64(y))
+        $Op(x::Float32, y::Single32) = $Op(Float64(x), Float64(y))
+        $Op(x::Single32, y::Float16) = $Op(Float64(x), Float64(y))
+        $Op(x::Float16, y::Single32) = $Op(Float64(x), Float64(y))
+        $Op(x::Single32, y::BigFloat) = $Op(Float64(x), Float64(y))
+        $Op(x::BigFloat, y::Single32) = $Op(Float64(x), Float64(y))
+        $Op(x::Single32, y::Int64) = $Op(Float64(x), Float64(y))
+        $Op(x::Int64, y::Single32) = $Op(Float64(x), Float64(y))
+        $Op(x::Single32, y::Int32) = $Op(Float64(x), Float64(y))
+        $Op(x::Int32, y::Single32) = $Op(Float64(x), Float64(y))
+        $Op(x::Single32, y::Int16) = $Op(Float64(x), Float64(y))
+        $Op(x::Int16, y::Single32) = $Op(Float64(x), Float64(y))
+        $Op(x::Single32, y::Int128) = $Op(Float64(x), Float64(y))
+        $Op(x::Int128, y::Single32) = $Op(Float64(x), Float64(y))
+    end
+end
