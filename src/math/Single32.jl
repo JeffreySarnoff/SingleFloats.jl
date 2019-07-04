db55775af8a4ec5876f6d0a780cf91e76c080cbb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-04 04:29:24 -0400

    Update Single32.jl

--- src/math/Single32.jl
+++ src/math/Single32.jl
@@ -1,11 +1,3 @@
-import Base: +, -, *, /, \, %, √, ÷
-import Base.Math: ^, sqrt, cbrt, ∛, div, rem, fld, mod, cld, hypot, min, max, minmax, rad2deg, deg2rad, mod2pi, rem2pi
-import Base: abs, abs2, sign, copysign, flipsign, floor, ceil, trunc, round, divrem, fldmod, clamp
-import Base: exp, exp2, exp10, expm1, log, log2, log10, log1p, 
-             sin, cos, tan, csc, sec, cot, asin, acos, atan, acsc, asec, acot,
-             sinh, cosh, tanh, csch, sech, coth, asinh, acosh, atanh, acsch, asech, acoth
-
-
 # unary
 for Op in (:+, :-, :sqrt, :cbrt, :rad2deg, :deg2rad, :mod2pi, :rem2pi, :abs, :abs2, :sign, :floor, :ceil, :trunc, :round)
     @eval $Op(x::Single32) = Single32($Op(Float64(x)))
