45a8e035cff6fa97be5a76637e6725435a4cdfb1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-04 04:27:43 -0400

    Update Single32.jl

--- src/math/Single32.jl
+++ src/math/Single32.jl
@@ -1,4 +1,3 @@
-import Base: cmp, ==, !=, >=, >, <=, <, isless, isequal
 import Base: +, -, *, /, \, %, √, ÷
 import Base.Math: ^, sqrt, cbrt, ∛, div, rem, fld, mod, cld, hypot, min, max, minmax, rad2deg, deg2rad, mod2pi, rem2pi
 import Base: abs, abs2, sign, copysign, flipsign, floor, ceil, trunc, round, divrem, fldmod, clamp
@@ -6,28 +5,6 @@ import Base: exp, exp2, exp10, expm1, log, log2, log10, log1p,
              sin, cos, tan, csc, sec, cot, asin, acos, atan, acsc, asec, acot,
              sinh, cosh, tanh, csch, sech, coth, asinh, acosh, atanh, acsch, asech, acoth
 
-# comparisons
-for Op in (:cmp, :(==), :(!=), :(>=), :(<=), :(>), :(<), :isless, :isequal)
-    @eval begin
-        $Op(x::Single32, y::Single32) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Single32, y::Float64) = Single32($Op(Float64(x), y))
-        $Op(x::Float64, y::Single32) = Single32($Op(x, Float64(y)))
-        $Op(x::Single32, y::Float32) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Float32, y::Single32) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Single32, y::Float16) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Float16, y::Single32) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Single32, y::BigFloat) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::BigFloat, y::Single32) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Single32, y::Int64) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Int64, y::Single32) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Single32, y::Int32) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Int32, y::Single32) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Single32, y::Int16) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Int16, y::Single32) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Single32, y::Int128) = Single32($Op(Float64(x), Float64(y)))
-        $Op(x::Int128, y::Single32) = Single32($Op(Float64(x), Float64(y)))
-    end
-end
 
 # unary
 for Op in (:+, :-, :sqrt, :cbrt, :rad2deg, :deg2rad, :mod2pi, :rem2pi, :abs, :abs2, :sign, :floor, :ceil, :trunc, :round)
