37785d6864b7bd0f7ab350ec04b3d41f0972fce4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-04 01:38:51 -0400

    Update linearalgebra.jl

--- src/math/linearalgebra.jl
+++ src/math/linearalgebra.jl
@@ -7,18 +7,17 @@ svd,
 
 for Op in (:iszero, :isone, :isdiag, :issymmetric,
            :ishermitian, :isposdef, :isposdef!, :istril, :istriu)
-    @eval $Op(x::Array{FloatWK,2}) = $Op(Float64.(x))
+    @eval $Op(x::Array{Single32,2}) = $Op(Float64.(x))
 end
 
 for Op in (:rank, :cond, :norm, :opnorm, :det, :tr)
-    @eval $Op(x::Array{FloatWK,2}) = FloatWK($Op(Float64.(x)))
+    @eval $Op(x::Array{Single32,2}) = Single32($Op(Float64.(x)))
 end
  
 for Op in (:transpose, :adjoint, :inv, :pinv)
-    @eval $Op(x::Array{FloatWK,2}) = FloatWK.($Op(Float64.(x)))
+    @eval $Op(x::Array{Single32,2}) = Single32.($Op(Float64.(x)))
 end
  
 for Op in (:eigvals, :eigvals!, :svdvals, :svdvals!)
-    @eval $Op(x::Array{FloatWK,2}) = FloatWK.($Op(Float64.(x)))
+    @eval $Op(x::Array{Single32,2}) = Single32.($Op(Float64.(x)))
 end
-
