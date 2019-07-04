db5700f98fdd2b388e28d07b1dd13dd0e7be934e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-04 04:30:07 -0400

    Update SingleFloats.jl

--- src/SingleFloats.jl
+++ src/SingleFloats.jl
@@ -4,6 +4,13 @@ export Single32
 
 using Base: IEEEFloat
 import Base: promote_rule, convert
+import Base: cmp, ==, !=, >=, >, <=, <, isless, isequal
+import Base: +, -, *, /, \, %, √, ÷
+import Base: ^, sqrt, cbrt, ∛, div, rem, fld, mod, cld, hypot, min, max, minmax, rad2deg, deg2rad, mod2pi, rem2pi
+import Base: abs, abs2, sign, copysign, flipsign, floor, ceil, trunc, round, divrem, fldmod, clamp
+import Base: exp, exp2, exp10, expm1, log, log2, log10, log1p, 
+             sin, cos, tan, csc, sec, cot, asin, acos, atan, acsc, asec, acot,
+             sinh, cosh, tanh, csch, sech, coth, asinh, acosh, atanh, acsch, asech, acoth
 
 using LinearAlgebra
 import LinearAlgebra: rank, cond, norm, opnorm, det, tr, inv, pinv, 
