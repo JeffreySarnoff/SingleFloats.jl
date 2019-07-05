1c1a329891aef99581e2d2f9031ab801f6871781
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-05 01:32:47 -0400

    Update Single32.jl

--- src/type/Single32.jl
+++ src/type/Single32.jl
@@ -34,54 +34,59 @@ Bool(x::Single32) = x==0 ? false : x==1 ? true : throw(InexactError(:Bool, Bool,
 
 Single32(x::Irrational{S}) where S = Single32(Float64(x))
 
-Base.show(io::IO, x::Single32) = show(io, Float32(x))
-Base.string(x::Single32) = string(Float32(x))
-Base.repr(x::Single32) = string("Single32(",repr(Float64(x)),")")
+show(io::IO, x::Single32) = show(io, Float32(x))
+string(x::Single32) = string(Float32(x))
+repr(x::Single32) = string("Single32(",repr(Float64(x)),")")
 
-Base.widen(::Type{Single32}) = Float64
-Base.widen(x::Single32) = Float64(x)
+widen(::Type{Single32}) = Float64
+widen(x::Single32) = Float64(x)
 
-Base.hash(x::Single32) = hash(Float64(x))
-Base.hash(x::Single32, h::UInt64) = hash(Float64(x), h)
+hash(x::Single32) = hash(Float64(x))
+hash(x::Single32, h::UInt64) = hash(Float64(x), h)
 
-Base.typemin(::Type{Single32})  = typemin(Float32)
-Base.typemax(::Type{Single32})  = typemax(Float32)
-Base.floatmin(::Type{Single32}) = floatmin(Float32)
-Base.floatmax(::Type{Single32}) = floatmax(Float32)
-Base.typemin(x::Single32)  = typemin(Single32)
-Base.typemax(x::Single32)  = typemax(Single32)
-Base.floatmin(x::Single32) = floatmin(Single32)
-Base.floatmax(x::Single32) = floatmax(Single32)
+typemin(::Type{Single32})  = typemin(Float32)
+typemax(::Type{Single32})  = typemax(Float32)
+floatmin(::Type{Single32}) = floatmin(Float32)
+floatmax(::Type{Single32}) = floatmax(Float32)
+typemin(x::Single32)  = typemin(Single32)
+typemax(x::Single32)  = typemax(Single32)
+floatmin(x::Single32) = floatmin(Single32)
+floatmax(x::Single32) = floatmax(Single32)
 
-Base.maxintfloat(::Type{Single32}) = maxintfloat(Float32)
-Base.maxintfloat(::Type{Single32}, ::Type{I}) where {I<:Integer} = maxintfloat(Float32, I)
+maxintfloat(::Type{Single32}) = maxintfloat(Float32)
+maxintfloat(::Type{Single32}, ::Type{I}) where {I<:Integer} = maxintfloat(Float32, I)
+
+zero(::Type{Single32}) = Single32(zero(Float64))
+one(::Type{Single32}) = Single32(one(Float64))
+iszero(x::Single32) = iszero(Float64(x))
+isone(x::Single32) = isone(Float64(x))
 
 prevfloat(x::Single32, n::Int) = Single32(prevfloat(Float32(x), n))
 nextfloat(x::Single32, n::Int) = Single32(nextfloat(Float32(x), n))
 
-Base.eps(::Type{Single32}) = eps(Float64)
-Base.exponent(x::Single32) = exponent(Float64(x))
-Base.significand(x::Single32) = significand(Float64(x))
+eps(::Type{Single32}) = eps(Float64)
+exponent(x::Single32) = exponent(Float64(x))
+significand(x::Single32) = significand(Float64(x))
 
 
-Base.trunc(x::Single32) = Core.Intrinsics.trunc_llvm(x)
-Base.floor(x::Single32) = Core.Intrinsics.floor_llvm(x)
-Base.ceil(x::Single32)  = Core.Intrinsics.ceil_llvm(x)
+trunc(x::Single32) = Core.Intrinsics.trunc_llvm(x)
+floor(x::Single32) = Core.Intrinsics.floor_llvm(x)
+ceil(x::Single32)  = Core.Intrinsics.ceil_llvm(x)
 
-Base.round(x::Single32, r::RoundingMode{:ToZero})  = Core.Intrinsics.trunc_llvm(x)
-Base.round(x::Single32, r::RoundingMode{:Down})    = Core.Intrinsics.floor_llvm(x)
-Base.round(x::Single32, r::RoundingMode{:Up})      = Core.Intrinsics.ceil_llvm(x)
-Base.round(x::Single32, r::RoundingMode{:Nearest}) = Core.Intrinsics.rint_llvm(x)
+round(x::Single32, r::RoundingMode{:ToZero})  = Core.Intrinsics.trunc_llvm(x)
+round(x::Single32, r::RoundingMode{:Down})    = Core.Intrinsics.floor_llvm(x)
+round(x::Single32, r::RoundingMode{:Up})      = Core.Intrinsics.ceil_llvm(x)
+round(x::Single32, r::RoundingMode{:Nearest}) = Core.Intrinsics.rint_llvm(x)
 
-Base.trunc(::Type{I}, x::Single32) where {I<:Integer} = (I)(Core.Intrinsics.trunc_llvm(x))
-Base.floor(::Type{I}, x::Single32) where {I<:Integer} = (I)(Core.Intrinsics.floor_llvm(x))
-Base.ceil(::Type{I}, x::Single32)  where {I<:Integer} = (I)(Core.Intrinsics.ceil_llvm(x))
+trunc(::Type{I}, x::Single32) where {I<:Integer} = (I)(Core.Intrinsics.trunc_llvm(x))
+floor(::Type{I}, x::Single32) where {I<:Integer} = (I)(Core.Intrinsics.floor_llvm(x))
+ceil(::Type{I}, x::Single32)  where {I<:Integer} = (I)(Core.Intrinsics.ceil_llvm(x))
 
-Base.round(::Type{I}, x::Single32, r::RoundingMode{:ToZero})  where {I<:Integer} = (I)(Core.Intrinsics.trunc_llvm(x))
-Base.round(::Type{I}, x::Single32, r::RoundingMode{:Down})    where {I<:Integer} = (I)(Core.Intrinsics.floor_llvm(x))
-Base.round(::Type{I}, x::Single32, r::RoundingMode{:Up})      where {I<:Integer} = (I)(Core.Intrinsics.ceil_llvm(x))
-Base.round(::Type{I}, x::Single32, r::RoundingMode{:Nearest}) where {I<:Integer} = (I)(Core.Intrinsics.rint_llvm(x))
-Base.round(::Type{I}, x::Single32) where {I<:Integer} = (I)(Core.Intrinsics.rint_llvm(x))
+round(::Type{I}, x::Single32, r::RoundingMode{:ToZero})  where {I<:Integer} = (I)(Core.Intrinsics.trunc_llvm(x))
+round(::Type{I}, x::Single32, r::RoundingMode{:Down})    where {I<:Integer} = (I)(Core.Intrinsics.floor_llvm(x))
+round(::Type{I}, x::Single32, r::RoundingMode{:Up})      where {I<:Integer} = (I)(Core.Intrinsics.ceil_llvm(x))
+round(::Type{I}, x::Single32, r::RoundingMode{:Nearest}) where {I<:Integer} = (I)(Core.Intrinsics.rint_llvm(x))
+round(::Type{I}, x::Single32) where {I<:Integer} = (I)(Core.Intrinsics.rint_llvm(x))
 
     
 Base.BigFloat(x::Single32) = BigFloat(Float64(x))
@@ -90,8 +95,8 @@ Base.Int64(x::Single32) = Int64(Float64(x))
 Base.Int32(x::Single32) = Int32(Float64(x))
 Base.Int16(x::Single32) = Int16(Float64(x))
 
-Base.promote_rule(::Type{Single32}, ::Type{Float32}) = Single32
-Base.promote_rule(::Type{Single32}, ::Type{Float64}) = Single32 
+promote_rule(::Type{Single32}, ::Type{Float32}) = Single32
+promote_rule(::Type{Single32}, ::Type{Float64}) = Single32 
 
  
 for T in (BigFloat, Float64, Float32, Float16,
