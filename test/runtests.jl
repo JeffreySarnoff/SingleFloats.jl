8498561f23ef7eaed1b3b087b5b24f5cb307f9b7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-04 20:39:34 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using SingleFloats
 using Test
 
-@test Float32(sqrt(Single32(2.0))) = Float32(sqrt(Float64(2.0)))
+@test Float64(sqrt(Single32(2))_ == sqrt(2.0)
+@test Float32(sqrt(Single32(2.0))) == Float32(sqrt(Float64(2.0)))
 
 @test sum(cot.(Single32.(collect(1.0:20)))) == sum(cot.(reverse(Single32.(collect(1.0:20)))))
