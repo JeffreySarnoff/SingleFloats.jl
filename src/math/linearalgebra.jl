827834fd900966a265d459737e50547a9557e2e1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-05 19:07:41 -0400

    add more linalg functions

--- src/math/linearalgebra.jl
+++ src/math/linearalgebra.jl
@@ -1,4 +1,4 @@
-for Op in (:iszero, :isone, :isdiag, :issymmetric,
+for Op in (:iszero, :isone, :isdiag, :issymmetric, :isdiag,
            :ishermitian, :isposdef, :isposdef!, :istril, :istriu)
     @eval $Op(x::Array{Single32,2}) = $Op(Float64.(x))
 end
@@ -10,7 +10,19 @@ end
 for Op in (:transpose, :adjoint, :inv, :pinv)
     @eval $Op(x::Array{Single32,2}) = Single32.($Op(Float64.(x)))
 end
- 
-for Op in (:eigvals, :eigvals!, :svdvals, :svdvals!)
+
+for Op in (:lu, :lu!, :qr, :qr!, :schur, :schur!)
     @eval $Op(x::Array{Single32,2}) = Single32.($Op(Float64.(x)))
+end
+
+for Op in (:cholesky, :cholesky!, :hessenberg, :hessenberg!, :factorize)
+    @eval $Op(x::Array{Single32,2}) = Single32.($Op(Float64.(x)))
+end
+
+for Op in (:eigvals, :eigvals!, :svdvals, :svdvals!, :svd)
+    @eval $Op(x::Array{Single32,2}) = Single32.($Op(Float64.(x)))
+end
+
+for Op in (:eigvals, :eigvecs, :svdvals, :svd)
+    @eval $Op(x::Array{Single32,2}, y::Array{Single32,2}) = Single32.($Op(Float64.(x), Float64.(y)))
 end
