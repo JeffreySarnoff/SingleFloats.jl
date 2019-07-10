ab1cee13b01b1f154b9e5bcb520def601eb8bea6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-10 10:04:53 -0400

    Update SingleFloats.jl

--- src/SingleFloats.jl
+++ src/SingleFloats.jl
@@ -6,7 +6,7 @@ using Base: IEEEFloat
 import Base: promote_rule, convert, hash, show, string, repr
 import Base: cmp, ==, !=, >=, >, <=, <, isless, isequal
 import Base: +, -, *, /, \, %, √, ÷, inv
-import Base: prevfloat, nextfloat, eps, exponent, significand, zero, one, iszero, isone, decompose
+import Base: prevfloat, nextfloat, eps, exponent, significand, zero, one, iszero, isone, isinteger, decompose
 import Base: typemin, typemax, floatmin, floatmax, maxintfloat, widen
 import Base: ^, sqrt, cbrt, ∛, div, rem, fld, mod, cld, hypot, min, max, minmax, rad2deg, deg2rad, mod2pi, rem2pi
 import Base: abs, abs2, sign, copysign, flipsign, floor, ceil, trunc, round, divrem, fldmod, clamp
