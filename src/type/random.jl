01db9f74ea8971b14ab7c2fa71aa08af5efc0911
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-12-28 04:35:48 -0500

    generalize rand for arrays

--- src/type/random.jl
+++ src/type/random.jl
@@ -1,2 +1,5 @@
 Random.rand(::Type{Single32}) = Single32(rand(Float32))
 Random.rand(::Type{Single32}, n::Integer=1) = map(Single32, rand(Float32, max(0,n)))
+Random.rand(::Type{Single32}, a=1,b=1) = map(Single32, rand(Float32, max(0,a), max(0, b)))
+Random.rand(::Type{Single32}, xs::Vararg{Int,N}) where {N} = map(Single32, rand(Float32, map(z->max(0,z), xs)...,))
+
