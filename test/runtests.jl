ae2597a88764184667a6e918be485344ea6c2acf
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-12-28 20:52:06 -0500

    include numerical_advantage.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,5 @@ include("frombase.jl")
 @test Float32(sqrt(Single32(2.0))) == Float32(sqrt(Float64(2.0)))
 
 #@test sum(cot.(Single32.(collect(1.0:20)))) == sum(cot.(reverse(Single32.(collect(1.0:20)))))
+
+include("numerical_advantage.jl")
